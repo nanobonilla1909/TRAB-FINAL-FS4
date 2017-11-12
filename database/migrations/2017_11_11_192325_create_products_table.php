bf2fec36fde4a6495f625cc9e0d553ce364d718d
Mariano Bonilla <marianobonilla@MacBook-Pro-de-Mariano.local>
2017-11-11 22:48:24 -0300

    Pantalla de Login con Estilos

--- database/migrations/2017_11_11_192325_create_products_table.php
+++ database/migrations/2017_11_11_192325_create_products_table.php
@@ -21,7 +21,7 @@ class CreateProductsTable extends Migration
             $table->float('price');
  
             // Foreign Key
-            $table->integer('category_id')->unsigned();
+            $table->integer('category_id')->unsigned()->nullable();
             $table->foreign('category_id')->references('id')->on('categories');
 
             $table->timestamps();
